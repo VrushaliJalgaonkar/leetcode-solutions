d1ef4fa2508c6395f7ce77729ae9212757ef8af7
Vrushali Jalgaonkar <vrushali.jalgaonkar16@gmail.com>
2023-12-14 19:20:11 +0900

    Create 136_Single_Number.java

---
+++ Bitwise/136_Single_Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int a = 0;
+        for (int i : nums) {
+            a ^= i;
+        }
+        return a;
+    }
+}
+//TC - O(n), SC - O(1)
