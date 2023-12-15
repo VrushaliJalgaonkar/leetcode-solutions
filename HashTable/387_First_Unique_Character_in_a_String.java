7d6fb44191eb0465ebaaa4f89c7d8d639c0d574e
Vrushali Jalgaonkar <vrushali.jalgaonkar16@gmail.com>
2023-12-15 16:33:56 +0900

    Create 387_First_Unique_Character_in_a_String.java

---
+++ HashTable/387_First_Unique_Character_in_a_String.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(int i=0;i<s.length();i++){
+            if(map.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+    }
+}
+//TC - O(n)
+//SC - O(n)
