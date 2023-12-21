60027c7fc92888cbaea9339f38224415d86ed403
Vrushali Jalgaonkar <vrushali.jalgaonkar16@gmail.com>
2023-12-21 18:18:46 +0900

    Create 1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points.java

---
+++ Sorting/1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (a,b) -> Integer.compare(a[0], b[0]));
+        int ans = 0;
+        for(int i=1; i< points.length;i++){
+            ans = Math.max(ans, points[i][0] - points[i-1][0]);
+        }
+        return ans;
+    }
+}
+//TC - O(n logn), SC - O(logn), because Arrays.sort() uses some space in java
